164d7f5dde021df99df260c76d335fafc4feb7aa
Shaun Overton <texasshaun@hotmail.com>
2014-07-24 20:04:37 -0500

    This commit contains my personal solution to homework assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix needs to run before cacheSolve. The purpose of makeCacheMatrix
+## is to receive a square matrix x and place it in a list object, which can then be accessed
+## by the functions get(), set(), getInverse(), setInverse().
 
-## Write a short comment describing this function
+## get() returns the square matrix
+## set() creates the square matrix when called from a makeCacheMatrix object (that's why it uses the <<- operator)
+## getInverse() looks for cached inverse of the square matrix
+## setInverse() is the function that will run from cacheSolve to create 
+##    the square matrix (that's why it uses the <<- operator)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  get <- function() x
+  
+  getInverse <- function() inverse
+  
+  # allow this function to set this value outside its scope by using <<- operator
+  setInverse <- function(solve) inverse <<- solve
+  
+  # return a list object
+  list( set=set, get=get, getInverse=getInverse, setInverse=setInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the value of the matrix if it's already been solved. Otherwise, the function uses the solve
+## method proposed in the homework instructions to do the calculation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getInverse()
+  
+  # return the solved matrix if the calculation has already been performed
+  
+  if( !(is.null(inverse))) {
+    message("getting the cached inverse")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  
+  # solve the inverse matrix
+  inverse <- solve(x$get())
+  
+  #pass it back to the makeCacheMatrix object
+  x$setInverse( inverse )
+  
+  inverse
 }
